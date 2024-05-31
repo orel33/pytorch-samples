6a44bbc0e799f6bd4e9dc72b20460814325c6a95
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2024-05-31 16:51:55 +0200

    compute edgecut metics

--- 03-gnn/demo.py
+++ 03-gnn/demo.py
@@ -13,7 +13,7 @@ import nxmetis
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import SAGEConv
-from torch_geometric.utils import degree
+from torch_geometric.utils import degree, from_networkx, to_networkx
 import matplotlib.pyplot as plt
 
 # Cpu by default
@@ -46,16 +46,17 @@ def grid_plot(nrows, ncols, part):
 
 ######################## Data ########################
 
+# Maybe use: https://pytorch-geometric.readthedocs.io/en/1.7.2/modules/utils.html#torch_geometric.utils.grid
 
 def grid_graph(nrows, ncols):
     # WARNING: the grid dimension are swapped!
     g = nx.grid_graph(dim=(ncols, nrows))  # networkx graph
-    adj_sparse = nx.to_scipy_sparse_array(g, format='coo')
-    row = adj_sparse.row
-    col = adj_sparse.col
-    rowcol = np.array([row, col])
-    edges = torch.tensor(rowcol, dtype=torch.long)
-    # append one hot encoding of the desired partition
+    # convert to pytorch geometric graph
+    data = from_networkx(g)
+    # append dummy node features
+    # data.x = torch.ones((g.number_of_nodes(), 1), dtype=torch.float)
+    # data.x = torch.rand((g.number_of_nodes(), 2), dtype=torch.float)
+    # append one-hot encoding of the desired partition
     one_hot = []
     for node in g.nodes():
         i, j = node[0], node[1]
@@ -64,12 +65,8 @@ def grid_graph(nrows, ncols):
             one_hot.append([0., 1.])  # partition 0
         else:
             one_hot.append([1., 0.])  # partition 1
-    nodes = torch.tensor(np.array(one_hot), dtype=torch.float)
-    # append dummy node features
-    # nodes = torch.ones((g.number_of_nodes(), 1), dtype=torch.float)
-    # nodes = torch.rand((g.number_of_nodes(), 1), dtype=torch.float)
-    G = Data(x=nodes, edge_index=edges)    # pytorch geometric graph
-    return G
+    data.x = torch.tensor(np.array(one_hot), dtype=torch.float)
+    return data
 
 ######################## Loss Function ########################
 
@@ -137,12 +134,11 @@ loader = DataLoader(dataset, batch_size=1, shuffle=True)
 loss_fn = loss_normalized_cut
 lr = 0.0001  # learning rate
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)  # optimizer
-epochs = 1000  # epochs
+epochs = 300  # epochs
 print_loss = 100  # steps after which the loss function is printed
 losses = []
 
 
-# epochs = 0
 # Training loop
 print('Start training')
 for i in range(epochs):
@@ -170,9 +166,12 @@ print(gg)
 data = model(gg)
 part = torch.argmax(data, dim=1)
 print('Model output:', data)
-print('Partition:', part)
+print('Prediction Partition:', part)
 print('Loss:', loss_fn(data, gg).item())
 print('')
+
+print("cut:", torch.sum(part[gg.edge_index[0, :]]
+      != part[gg.edge_index[1, :]]).item() / 2)
 
 # Plot output
 grid_plot(nrows, ncols, part)
--- 03-gnn/metis/metrics.py
+++ 03-gnn/metis/metrics.py
@@ -1,28 +1,85 @@
 import networkx as nx
 import nxmetis
 import timeit
+import torch
+
+# convert partition from networkx to list
+
+# nxpart: [ [list of nodes in partition 0], [list of nodes in partition 1] ]
+# nxpart is the output of nxmetis.partition
+
+# part: [ partition of node 0, partition of node 1, ...] as a list
+
+# edge_index: [ [source nodes], [target nodes] ] as a list of lists
 
 
-# Returns the normalized cut, runtime, volumes and cut of the METIS
-# partitioning of the networkx graph g
+def nxpart2list(g, nxpart):
+    dictpart = {}
+    for p, nodes in enumerate(nxpart):
+        for n in nodes:
+            dictpart[n] = p
+    part = [dictpart[n] for n in g.nodes()]
+    return part
+
+# cut for networkx graph
 
 
-def normalized_cut_metis(g):
-    t0 = timeit.default_timer()
-    cut, parts = nxmetis.partition(g, 2)
-    t1 = timeit.default_timer() - t0
-    degA = sum(g.degree(n) for n in parts[0])
-    degB = sum(g.degree(n) for n in parts[1])
-    return cut * (1 / degA + 1 / degB), t1, degA, degB, cut
-
-# Returns the cut of the partitioning of the pytorch geometric graph
+def nx_cut(g, part):
+    cut = 0
+    for u, v in g.edges():
+        print("u:", u, "v:", v)
+        if part[u] != part[v]:
+            cut += 1
+    return cut / 2
 
 
-def cut(graph):
-    cut = torch.sum((graph.x[graph.edge_index[0],
-                             :2] != graph.x[graph.edge_index[1],
-                                            :2]).all(axis=-1)).detach().item() / 2
+# normalized cut for networkx graph
+
+def nx_ncut(g, part):
+    cut = nx_cut(g, part)
+    degA = sum(g.degree(n) for n in part[0])
+    degB = sum(g.degree(n) for n in part[1])
+    return cut * (1 / degA + 1 / degB)
+
+# normalized cut for pytorch geometric graph
+
+
+def pyg_ncut(y_pred, graph):
+    y = y_pred
+    d = degree(graph.edge_index[0], num_nodes=y.size(0))
+    gamma = y.t() @ d
+    c = torch.sum(y[graph.edge_index[0], 0] * y[graph.edge_index[1], 1])
+    return torch.sum(torch.div(c, gamma))
+
+# cut for pytorch geometric graph
+
+
+# def pyg_cut(graph):
+#     cut = torch.sum((graph.x[graph.edge_index[0], :2] != graph.x[graph.edge_index[1], :2]).all(
+#         axis=-1)).detach().item() / 2
+#     return cut
+
+def pyg_cut(g, part):
+    cut = torch.sum(part[g.edge_index[0, :]] !=
+                    part[g.edge_index[1, :]]).item() / 2
     return cut
 
 
-# TODO: try it
+########################### main ###########################
+nrows = ncols = 10
+g = nx.grid_graph(dim=(ncols, nrows))  # networkx graph
+t0 = timeit.default_timer()
+mcut, nxpart = nxmetis.partition(g, 2)
+t1 = timeit.default_timer() - t0
+print("METIS time:", t1)
+print("METIS cut:", mcut)
+part = nxpart2list(g, nxpart)
+print("METIS part:", part)
+# cut = nx_cut(g, part)
+# ncut = nx_ncut(g, part)
+cut = nx.cut_size(g, nxpart[0], nxpart[1])
+print("cut:", cut)
+ncut = nx.normalized_cut_size(g, nxpart[0], nxpart[1])
+print("ncut:", ncut)
+
+# EOF
